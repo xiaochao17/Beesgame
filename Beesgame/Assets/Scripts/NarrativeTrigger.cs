ff71c07e5699985c0603fb54e92b32064b5d50fe
ZiqianPei <ziqian.pei@gmail.com>
2019-01-27 08:19:50 -0800

    narrativeTrigger script

---
+++ Beesgame/Assets/Scripts/NarrativeTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrativeTrigger : MonoBehaviour
+{
+
+    private GameObject player;
+
+    private float moveDistance;
+    private float moveSpeed;
+    private float startTime;
+    private float elapsedTime;
+    private Vector2 startPoint;
+    private Vector2 endPoint;
+    private Quaternion startAngle;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+            player.GetComponent<PlayerMovement>().enabled = false;
+            startAngle = player.transform.rotation;
+            startPoint = player.transform.position;
+            moveDistance = gameObject.transform.parent.transform.position.y - player.transform.position.y;
+            endPoint = startPoint + new Vector2(0.0f, moveDistance);
+            startTime = Time.time;
+            StartCoroutine("movePlayer");
+        }
+    }
+
+    IEnumerator movePlayer()
+    {
+        elapsedTime = 0.0f;
+        while (elapsedTime < 1.0f)
+        {
+            player.transform.position = Vector2.Lerp(startPoint, endPoint, elapsedTime);
+            player.transform.rotation = Quaternion.Lerp(startAngle, Quaternion.identity, elapsedTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void Update()
+    {
+        if (elapsedTime >= 1)
+        {
+            Debug.Log("Hi");
+        }
+    }
+
+
+}
