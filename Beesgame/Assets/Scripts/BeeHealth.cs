913f3dc947ded8e6db8c4b54080c817073cfdf6c
Daniel Son <danielsonjh@hotmail.com>
2019-01-26 23:00:10 -0800

    Made bee transparent while invulnerable

--- Beesgame/Assets/Scripts/BeeHealth.cs
+++ Beesgame/Assets/Scripts/BeeHealth.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class BeeHealth : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private GameObject healthBar;
     [SerializeField] private int maxValue;
     private int _value;
@@ -23,6 +24,10 @@ public class BeeHealth : MonoBehaviour
     void Update()
     {
         _damageCooldown.AddRemainingSeconds(-Time.deltaTime);
+        
+        var color = spriteRenderer.color;
+        color.a = _damageCooldown.CanUse() ? 1 : 0.5f;
+        spriteRenderer.color = color;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
