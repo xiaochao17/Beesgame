525d2b4ec5227b8d62c713bf8ea38f65c4a1f28b
ZiqianPei <ziqian.pei@gmail.com>
2019-01-26 16:32:54 -0800

    animation and movement

--- Beesgame/Assets/Scripts/PlayerMovement.cs
+++ Beesgame/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     public float forceAmount = 1.0f;
     public float dashForceModifier = 5.0f;
     public float tilt = 1.0f;
+    public float tiltThreshold = 25f;
+
 
 
     void Start()
@@ -37,6 +39,15 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             playerRB2D.AddForce(forceAmount * dashForceModifier * new Vector2(movement.x, 0), ForceMode2D.Impulse);
+        }
+
+        Debug.Log(playerRB2D.rotation);
+
+        if (Mathf.Abs(moveHorizontal) > Mathf.Epsilon && playerRB2D.rotation * Mathf.Sign(transform.localScale.x) > tiltThreshold)
+        {
+            var localScale = transform.localScale;
+            localScale.x = moveHorizontal > 0 ? 1 : -1;
+            transform.localScale = localScale;
         }
 
     }
