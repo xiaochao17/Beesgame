a3ed7dc77506de3cb6185a326a012caaf6c2acd4
Daniel Son <danielsonjh@hotmail.com>
2019-01-27 16:14:17 -0800

    Added ending scene + code

---
+++ Beesgame/Assets/Scripts/EndingSlideshow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingSlideshow : MonoBehaviour
+{
+    private const float FadeDuration = 0.25f;
+    private readonly float[] _slideDurations = { 2f, 2f };
+    private Image[] _images;
+    
+    void Start()
+    {
+        _images = GetComponentsInChildren<Image>();
+        for (var i = 0; i < _images.Length; i++)
+        {
+            _images[i].canvasRenderer.SetColor(Color.clear);
+        }
+        StartCoroutine(StartSlideshow());
+    }
+
+    IEnumerator StartSlideshow()
+    {
+
+
+        for (var i = 0; i < _images.Length; i++)
+        {
+            print(_images[i].canvasRenderer.GetColor());
+            _images[i].CrossFadeColor(Color.white, FadeDuration, false, true);
+            yield return new WaitForSeconds(_slideDurations[i]);
+        } 
+    }
+}
